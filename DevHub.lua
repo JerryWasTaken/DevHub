92f78d2dc0fd02a412a98fbea2c21e55e2a3050b
JerryWasTaken <99751332+JerryWasTaken@users.noreply.github.com>
2022-02-18 22:15:20 +0100

    Create DevHub.lua

---
+++ DevHub.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 4042427666 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/JerryWasTaken/DevHub/main/AnimeFightingSimulator.lua"))()
+	elseif game.PlaceId == 606849621 then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/JerryWasTaken/DevHub/main/Jailbreak.lua"))()
+	elseif game.PlaceId == 606849621 then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/JerryWasTaken/DevHub/main/NinjaLegends.lua"))()
+end
